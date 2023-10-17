9bee5bbc237d265a0b6c01a6d45f76a6a2f7c1a1
lenapapuc <elenapapuc99@gmail.com>
2023-10-17 23:13:31 +0300

    First Page

--- EndavaTechCourseBankingApplication/EndavaTechCourseBankingApplication/Client/Program.cs
+++ EndavaTechCourseBankingApplication/EndavaTechCourseBankingApplication/Client/Program.cs
@@ -1,6 +1,7 @@
 using EndavaTechCourseBankingApplication.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace EndavaTechCourseBankingApplication.Client
 {
@@ -12,9 +13,11 @@ namespace EndavaTechCourseBankingApplication.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
+        
     }
 }
